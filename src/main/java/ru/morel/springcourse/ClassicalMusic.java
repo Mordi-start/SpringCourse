ba408228a6f86db0da0335f40affdddb3714d538
dmitry_morel <dimc1998@gmail.com>
2021-02-27 17:33:24 +0300

    XML with Annotation

--- src/main/java/ru/morel/springcourse/ClassicalMusic.java
+++ src/main/java/ru/morel/springcourse/ClassicalMusic.java
@@ -1,5 +1,8 @@
 package ru.morel.springcourse;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClassicalMusic implements Music{
     @Override
     public String getSong() {
